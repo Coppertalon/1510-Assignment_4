7259a3b4108cbd1c8ccbe98cfc4e5c911a7cc3fc
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 15:23:10 -0800

    add test cases

---
+++ map_and_user_tests/test_move_decider_map_and_user.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from unittest.mock import patch
+import io
+from map_and_user import move_decider
+
+class Test(TestCase):
+
+    @patch('builtins.input', side_effect=['5'])
+    def test_move_decider_quit(self, mock_input):
+        output = move_decider({})
+        self.assertEqual(output, False)
+
+    @patch('builtins.input', side_effect=['6', '5'])
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_move_decider_invalid(self, get_output, mock_input):
+        move_decider({})
+        output = get_output.getvalue()
+        expected_output = '" 6 "  is an invalid movement. Please try again'
+        self.assertIn(expected_output, output)
---
+++ test_player_mover_map_and_user.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from unittest.mock import patch
+import io
+from map_and_user import player_mover
+
+class Test(TestCase):
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_player_mover_invalid(self, get_output):
+        player_mover({"player_position": [0, 1]}, "1")
+        output = get_output.getvalue()
+        expected_output = '" 1 "  is an invalid movement. Please try again'
+        self.assertIn(expected_output, output)
+
+    def test_player_mover_north(self):
+        player_pos = {"player_position": [1, 1]}
+        player_mover(player_pos, "1")
+        expected_output = {"player_position": [0, 1]}
+        self.assertEqual(player_pos["player_position"], expected_output["player_position"])
+
+    def test_player_mover_south(self):
+        player_pos = {"player_position": [1, 1]}
+        player_mover(player_pos, "2")
+        expected_output = {"player_position": [2, 1]}
+        self.assertEqual(player_pos["player_position"], expected_output["player_position"])
+
+    def test_player_mover_west(self):
+        player_pos = {"player_position": [1, 1]}
+        player_mover(player_pos, "3")
+        expected_output = {"player_position": [1, 0]}
+        self.assertEqual(player_pos["player_position"], expected_output["player_position"])
+
+    def test_player_mover_east(self):
+        player_pos = {"player_position": [1, 1]}
+        player_mover(player_pos, "4")
+        expected_output = {"player_position": [1, 2]}
+        self.assertEqual(player_pos["player_position"], expected_output["player_position"])
