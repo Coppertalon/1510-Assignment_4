79e569871ccca50431af78e9efdf1daf4d2cc207
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 14:28:15 -0800

    improve unit tests

--- gameplay_tests/test_game_choices_gameplay.py
+++ gameplay_tests/test_game_choices_gameplay.py
@@ -9,7 +9,7 @@ class Test(TestCase):
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['1'])
     def test_game_choices_roll(self, mock_input, mock_random):
-        total, roll, action, character = game_choices({"name": "test",
+        total, roll, action = game_choices({"name": "test",
                         "player_position": [4, 0],
                         "health": 3,
                         "level": 1,
@@ -23,21 +23,21 @@ class Test(TestCase):
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['2'])
     def test_game_choices_re_roll(self, mock_input, mock_random):
-        total, roll, action, character = game_choices({"name": "test",
+        total, roll, action = game_choices({"name": "test",
                                                        "player_position": [4, 0],
                                                        "health": 3,
                                                        "level": 1,
                                                        "exp": 0,
-                                                       "add": 0,
-                                                       "take_away": 0,
-                                                       "re_roll": 0}, 10, 3)
+                                                       "add": 1,
+                                                       "take_away": 1,
+                                                       "re_roll": 1}, 10, 3)
 
         self.assertEqual(total, 12)
 
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['3'])
     def test_game_choices_hold(self, mock_input, mock_random):
-        total, roll, action, character = game_choices({"name": "test",
+        total, roll, action = game_choices({"name": "test",
                                                        "player_position": [4, 0],
                                                        "health": 3,
                                                        "level": 1,
@@ -51,29 +51,29 @@ class Test(TestCase):
 
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['4'])
-    def test_game_choices_re_roll(self, mock_input, mock_random):
-        total, roll, action, character = game_choices({"name": "test",
+    def test_game_choices_add(self, mock_input, mock_random):
+        total, roll, action = game_choices({"name": "test",
                                                        "player_position": [4, 0],
                                                        "health": 3,
-                                                       "level": 1,
+                                                       "level": 2,
                                                        "exp": 0,
-                                                       "add": 0,
-                                                       "take_away": 0,
-                                                       "re_roll": 0}, 10, 3)
+                                                       "add": 1,
+                                                       "take_away": 1,
+                                                       "re_roll": 1}, 10, 4)
 
         self.assertEqual(total, 11)
 
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['5'])
-    def test_game_choices_re_roll(self, mock_input, mock_random):
-        total, roll, action, character = game_choices({"name": "test",
+    def test_game_choices_take_away(self, mock_input, mock_random):
+        total, roll, action = game_choices({"name": "test",
                                                        "player_position": [4, 0],
                                                        "health": 3,
-                                                       "level": 1,
+                                                       "level": 2,
                                                        "exp": 0,
-                                                       "add": 0,
-                                                       "take_away": 0,
-                                                       "re_roll": 0}, 10, 3)
+                                                       "add": 1,
+                                                       "take_away": 1,
+                                                       "re_roll": 1}, 10, 5)
 
         self.assertEqual(total, 9)
 
@@ -81,14 +81,14 @@ class Test(TestCase):
     @patch('random.randint', return_value=5)
     @patch('builtins.input', side_effect=['5'])
     def test_game_choices_re_roll(self, mock_input, mock_random, get_output):
-        total, roll, action, character = game_choices({"name": "test",
+        total, roll, action, = game_choices({"name": "test",
                                                        "player_position": [4, 0],
                                                        "health": 3,
                                                        "level": 1,
                                                        "exp": 0,
-                                                       "add": 0,
-                                                       "take_away": 0,
-                                                       "re_roll": 0}, 10, 3)
+                                                       "add": 1,
+                                                       "take_away": 1,
+                                                       "re_roll": 1}, 10, 3)
         output = get_output.getvalue()
         self.assertIn("bad choice\n", output)
         self.assertEqual(total, 10)
