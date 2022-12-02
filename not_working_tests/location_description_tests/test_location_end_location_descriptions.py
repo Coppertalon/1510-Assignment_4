db1813c2e58a2741960384a51a105a464602797b
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 20:27:06 -0800

    update comment description

---
+++ not_working_tests/location_description_tests/test_location_end_location_descriptions.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    # the function relies on user gameplay loop and is too complicated cannot be tested
+    def test_location_end(self):
+        self.fail()
