a624eef1b799adfeaae5a766cfa1fa7963a015f4
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 21:05:42 -0800

    cleaned up docstrings

--- location_descriptions.py
+++ location_descriptions.py
@@ -11,16 +11,14 @@ def location_start_back() -> None:
 
     """
     print the description of the start then return the player
-    
-    :param maps: a dictionary
-    :param player_character: a dictionary
+
     :precondition: player_character and maps are set dictionaries and will have all needed values
     :postcondition: inform the user they have returned to the start and return them to the move menu
     :return: dictionary, dictionary, int
     """
     print("start")
     input("press Enter to continue")
-    return 0
+    return
 
 
 def location_port_1(maps:
@@ -172,7 +170,7 @@ def location_easy_3(maps:
     stop = location_callers.location_check_easy(maps, player_character, done, location_easy_3)
 
     if stop:
-        return maps, player_character, 0, False
+        return 0, False
     map_and_user.player_stats(player_character)
 
     outcome, quitter = location_callers.combat_location(maps, player_character, location_easy_3, difficulty=16)
@@ -451,7 +449,7 @@ def location_difficult_2(maps:
 
     map_and_user.player_stats(player_character)
 
-    outcome, quitter =location_callers.combat_location(maps, player_character, location_difficult_2, difficulty=17)
+    outcome, quitter = location_callers.combat_location(maps, player_character, location_difficult_2, difficulty=17)
 
     return outcome, quitter
 
@@ -481,7 +479,7 @@ def location_difficult_3(maps:
     stop = location_callers.location_check_hard(maps, player_character, done, location_difficult_3)
 
     if stop:
-        return maps, player_character, 0, False
+        return 0, False
 
     map_and_user.player_stats(player_character)
 
@@ -745,9 +743,7 @@ def location_yawning_3(maps:
     return outcome, quitter
 
 
-def location_end(maps:
-                 dict[str: list, str: list, str: dict[str: list, str: list, str: list, str: list, str: list]],
-                 player_character:
+def location_end(player_character:
                  dict[str: str, str: tuple, str: int, str: int, str: int, str: int, str: int, str: int]) -> \
                  tuple[int, bool]:
     """
@@ -756,7 +752,6 @@ def location_end(maps:
     the player will fight the boss in a best two out of three, with the roll to beat increasing the more they win.
     after the game ends it will call a function to inform the player of the outcome.
     
-    :param maps: dictionary
     :param player_character: dictionary
     :precondition: player_character and maps are set dictionaries and will have all needed values
     :postcondition: have a score for the battle and call the function to inform player of outcome 
