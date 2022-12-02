c51d220982c2a39ae8d3ad3614e6e4649ca8ccb1
Coppertalon <brogers42@my.bcit.ca>
2022-12-02 09:15:50 -0800

    update tests with game writing

--- game_tests/test_game_lose.py
+++ game_tests/test_game_lose.py
@@ -8,10 +8,15 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_lose_print(self, get_output):
-        lose()
+        player = {"name": "test"}
+        lose(player)
         output = get_output.getvalue()
-        self.assertEqual(output, "lose text\n")
+        expected_output = "Having faced several losses to the fickle hands of fate test has lost too much credibility" \
+                          " in the town of Waterdeep. Your dreams of sailing the seas as a captain are not dissuaded " \
+                          "and you being you venture to a further port in hopes that the chance to prove yourself " \
+                          "is just around the corner.\nYou Lose\n"
+        self.assertIn(expected_output, output)
 
     def test_lose_return(self):
-        output = lose()
+        output = lose({"name": "test"})
         self.assertEqual(output, False)
--- game_tests/test_game_win.py
+++ game_tests/test_game_win.py
@@ -8,10 +8,14 @@ class Test(TestCase):
 
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_win_print(self, get_output):
-        win()
+        win({"name": "test"})
         output = get_output.getvalue()
-        self.assertEqual(output, "win text\n")
+        expected_output = "Having defeated the famous adventurer Volo, test has proven themselves as a capable leader" \
+                          " and earned the reputation necessary to captain a vessel." \
+                          " Able to achieve their dream of sailing the oceans wide, you walk back into the city," \
+                          " grinning as your mind fills with tales of the sea\nYou Win\n"
+        self.assertIn(expected_output, output)
 
     def test_win_return(self):
-        output = win()
+        output = win({"name": "test"})
         self.assertEqual(output, False)
