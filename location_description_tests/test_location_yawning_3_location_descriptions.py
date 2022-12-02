1fd3ce75792472ac3e756f455a0c956264dadde2
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 20:52:30 -0800

    add unit tests

---
+++ location_description_tests/test_location_yawning_3_location_descriptions.py
@@ -0,0 +1,93 @@
+from unittest import TestCase
+import location_callers
+from unittest.mock import patch
+from location_descriptions import location_yawning_3
+import io
+
+class Test(TestCase):
+
+    @patch('builtins.input', side_effect=['1'])
+    def test_location_location_yawning_3_not_done_move(self, mock_input):
+        maps = {"map_visual": [["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"]],
+                "map_locations": [["3", "3", "3", "y", "4"],
+                                  ["c", "c", "3", "y", "y"],
+                                  ["2", "2", "c", "3", "3"],
+                                  ["p", "2", "2", "c", "3"],
+                                  ["1", "p", "2", "c", "3"]]}
+        player_character = {"name": "test",
+                            "player_position": [4, 0],
+                            "health": 3,
+                            "level": 2,
+                            "exp": 0,
+                            "add": 0,
+                            "take_away": 0,
+                            "re_roll": 0}
+
+        output = location_yawning_3(maps, player_character, False)
+        self.assertEqual(output, (0, True))
+        self.assertEqual(maps["map_visual"], [["*", "*", "*", "*", "*"],
+                                              ["*", "*", "*", "*", "*"],
+                                              ["*", "*", "*", "*", "*"],
+                                              ["*", "*", "*", "*", "*"],
+                                              ["Y", "*", "*", "*", "*"]])
+        self.assertEqual(maps["map_locations"], [["3", "3", "3", "y", "4"],
+                                                 ["c", "c", "3", "y", "y"],
+                                                 ["2", "2", "c", "3", "3"],
+                                                 ["p", "2", "2", "c", "3"],
+                                                 [location_yawning_3, "p", "2", "c", "3"]])
+
+    @patch('builtins.input', side_effect=['2'])
+    def test_location_location_yawning_3_not_done_quit(self, mock_input):
+        maps = {"map_visual": [["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"],
+                               ["*", "*", "*", "*", "*"]],
+                "map_locations": [["3", "3", "3", "y", "4"],
+                                  ["c", "c", "3", "y", "y"],
+                                  ["2", "2", "c", "3", "3"],
+                                  ["p", "2", "2", "c", "3"],
+                                  ["1", "p", "2", "c", "3"]]}
+        player_character = {"name": "test",
+                            "player_position": [4, 0],
+                            "health": 3,
+                            "level": 2,
+                            "exp": 0,
+                            "add": 0,
+                            "take_away": 0,
+                            "re_roll": 0}
+
+        output = location_yawning_3(maps, player_character, False)
+        self.assertEqual(output, (0, False))
+
+    # cannot check text displayed in outside functions
+    # @patch('builtins.input', side_effect=['1'])
+    # @patch('sys.stdout', new_callable=io.StringIO)
+    # def test_location_location_yawning_3_found(self, get_output, mock_input):
+    #     maps = {"map_visual": [["*", "*", "*", "*", "*"],
+    #                            ["*", "*", "*", "*", "*"],
+    #                            ["*", "*", "*", "*", "*"],
+    #                            ["*", "*", "*", "*", "*"],
+    #                            ["*", "*", "*", "*", "*"]],
+    #             "map_locations": [["3", "3", "3", "y", "4"],
+    #                               ["c", "c", "3", "y", "y"],
+    #                               ["2", "2", "c", "3", "3"],
+    #                               ["p", "2", "2", "c", "3"],
+    #                               ["1", "p", "2", "c", "3"]]}
+    #     player_character = {"name": "test",
+    #                         "player_position": [4, 0],
+    #                         "health": 3,
+    #                         "level": 2,
+    #                         "exp": 0,
+    #                         "add": 0,
+    #                         "take_away": 0,
+    #                         "re_roll": 0}
+    #
+    #     location_yawning_3(maps, player_character, True)
+    #     output = get_output.getvalue()
+    #     expected_output = "explored"
+    #     self.assertIn(expected_output, output)
