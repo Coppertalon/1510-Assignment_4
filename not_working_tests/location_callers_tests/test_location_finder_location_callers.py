a2a21aefdf8a0f9516aebeea06d2df3d5e9ab1f3
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 20:32:18 -0800

    update comment description

--- not_working_tests/location_callers_tests/test_location_finder_location_callers.py
+++ not_working_tests/location_callers_tests/test_location_finder_location_callers.py
@@ -3,6 +3,6 @@ from unittest import TestCase
 
 class Test(TestCase):
 
-    # calls to functions that call outside modules and is too complex to test
+    #  this program relies on calling randomized functions and is too difficult to test
     def test_location_finder(self):
         self.fail()
