20692ec7023650081142fb33b5756e66e015705e
Coppertalon <brogers42@my.bcit.ca>
2022-12-01 18:10:07 -0800

    move to correct locations

---
+++ not_working_tests/test_location_finder_location_callers.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+
+    # calls to functions that call outside modules and is too complex to test
+    def test_location_finder(self):
+        self.fail()
